4730d07c6401405de649931507121abfbdbb75d5
Ahmed EL-Dakhakhny <>
2020-06-27 21:53:45 +0200

    noises with slider to control sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +9,8 @@ void main() {
 
 // TODO mute all
 // TODO resume all that was playing
-//
+// TODO clean unwanted files
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -49,7 +52,7 @@ class _SoundsPageState extends State<SoundsPage> {
     );
     noises.add(
       NoiseModel("sea waves", 'assets/images/sun-and-sea-100.png', Colors.blue,
-          'assets/audios/noiser_waves.mp3.mp3'),
+          'assets/audios/noiser_waves.mp3'),
     );
     noises.add(
       NoiseModel("night", 'assets/images/moon-and-stars-100.png', Colors.blue,
@@ -70,38 +73,90 @@ class _SoundsPageState extends State<SoundsPage> {
 List<Widget> generateNoisesList(List<NoiseModel> models) {
   List<Widget> widgets = List<Widget>();
   for (var model in models) {
-    widgets.add(generateButton(model));
+    widgets.add(Noise(model));
   }
   return widgets;
 }
 
-Widget generateButton(NoiseModel noiseModel) {
-  return FlatButton(
-    padding: EdgeInsets.all(5.0),
-    color: noiseModel.color,
-    child: Image.asset(noiseModel.imagePath),
-    onPressed: () {
-      print("onpressed");
-      noiseModel.player.playOrPause();
-    },
-  );
+class Noise extends StatefulWidget {
+  NoiseModel noiseModel;
+  Noise(this.noiseModel);
+  @override
+  _NoiseState createState() => _NoiseState(this.noiseModel);
+}
+
+class _NoiseState extends State<Noise> {
+  NoiseModel noiseModel;
+  _NoiseState(this.noiseModel);
+  @override
+  Widget build(BuildContext context) {
+    print(noiseModel.volume);
+    return Container(
+      height: 150,
+      width: double.infinity,
+      child: Stack(fit: StackFit.loose, children: [
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: FlatButton(
+            color: noiseModel.color,
+            child: Image.asset(noiseModel.imagePath),
+            onPressed: () {
+              print("onpressed");
+              noiseModel.player.playOrPause();
+            },
+          ),
+        ),
+        Visibility(
+          visible: true,
+          child: Positioned(
+            right: 0,
+            child: Transform.rotate(
+              angle: 3 * pi / 2,
+              child: Slider(
+                activeColor: Colors.white,
+                inactiveColor: Colors.white,
+                value: noiseModel.volume,
+                onChanged: (newVal) {
+                  print(newVal);
+                  setState(() {
+                    noiseModel.volume = newVal;
+                  });
+                },
+              ),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
 }
 
 class NoiseModel {
+  static const double defaultVolume = 0.5;
   AssetsAudioPlayer player;
   Color color;
   String name;
   String imagePath;
   String audioUrl;
+
   NoiseModel(String name, String imagePath, Color color, String audioUrl) {
     this.name = name;
     this.imagePath = imagePath;
     this.color = color;
     this.audioUrl = audioUrl;
-    this.player = AssetsAudioPlayer();
-    player.open(
-      Audio(audioUrl),
-      autoStart: false,
-    );
+    this.player = AssetsAudioPlayer.newPlayer();
+    player.open(Audio(audioUrl), autoStart: false, volume: defaultVolume);
+  }
+  set volume(double v) {
+    player.setVolume(v);
+  }
+
+  double get volume {
+    return player.volume.value;
+  }
+
+  bool get isPlaying {
+    return player.isPlaying.value;
   }
 }
